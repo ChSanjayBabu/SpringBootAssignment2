b8e03850ac9bca7a3d0dc18941434eb6962566a1
ChSanjayBabu <sanjay.ch222@gmail.com>
2019-10-17 11:31:20 +0530

    properties added

--- src/main/java/com/stackroute/Muzix/seeder/MovieSeeder.java
+++ src/main/java/com/stackroute/Muzix/seeder/MovieSeeder.java
@@ -3,12 +3,23 @@ package com.stackroute.Muzix.seeder;
 import com.stackroute.Muzix.domain.Track;
 import com.stackroute.Muzix.repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+@PropertySource("application.properties")
 @Component
 public class MovieSeeder {
+
+    @Value("${id}")
+    String id;
+    @Value("${trackName}")
+    String trackName;
+    @Value("${trackComment}")
+    String trackComment;
     TrackRepository trackRepository;
 
     @Autowired
@@ -23,9 +34,9 @@ public class MovieSeeder {
     private void seedTrackTable()
     {
         Track track =new Track();
-        track.setTrackId(1);
-        track.setTrackName("Closer");
-        track.setComments("Good song");
+        track.setTrackId(Integer.parseInt(id));
+        track.setTrackName(trackName);
+        track.setComments(trackComment);
         trackRepository.save(track);
     }
 }
